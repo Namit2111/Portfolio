f8ddb21591b2a2703e5ed22a4cabe20f71d25a75
Nmait2111 <namitjain2111@gmail.com>
2023-04-03 00:24:26 +0530

    f

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+   
   'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
