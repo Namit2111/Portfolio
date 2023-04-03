ef0c5ebdae7158a551c0d7c247bb4f91e42f4891
Nmait2111 <namitjain2111@gmail.com>
2023-04-03 16:23:33 +0530

    fianl

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles_build', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
